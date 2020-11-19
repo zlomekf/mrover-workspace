0e25ffd202d8d05a248de66b60c5eab7fb5f8044
zlomekf <zlomekf@umich.edu>
2020-11-18 21:25:02 -0500

    read and write functions

--- jetson/mobility_bridge/__main__.py
+++ jetson/mobility_bridge/__main__.py
@@ -2,34 +2,48 @@ import serial
 #import lcm
 #from rover_msgs import AutonState, NavStatus
 import Adafruit_BBIO.UART as UART
-import numpy as np
-import struct
+import time
 
-
+#TODO
+'''
+functions for setting and reading individual registers? readability, style
+correct sleep time and timing invariants
+program control flow
+error handling
+LCM integration
+'''
+#defines
 baud = 115200
+uartPort = "/dev/ttyS4"
+
+def serialWrite(command, data):
+    with serial.Serial(port= uartPort, baudrate=baud) as ser:       #matching serial declaration
+        buf = ((data <<8) | (0xFF & command))                       #create a 2 byte buffer to send 
+        buf = int.to_bytes(buf, 2, byteorder='little', signed=True) #convert to 2 byte Little Endian
+        ser.write(buf)
+    
+def serialRead(command):
+    with serial.Serial(port= uartPort, baudrate=baud) as ser:            #matching serial declaration
+        buf = int.to_bytes(command, 1, byteorder= 'little', signed=True) #create input byte buffer
+        return ser.read(buf)                                             #return read value
 
 
 def main():
     UART.setup("UART4")
-    #ttyS4 board specific or how determined?
-    with serial.Serial(port="/dev/ttyS4", baudrate=baud) as ser:
+    with serial.Serial(port= uartPort, baudrate=baud) as ser:
         ser.close()
         ser.open()
         while 1:
             try:
-                # ser.write(int.to_bytes(-100, 1, byteorder='little', signed=True))
-                rpm = np.int16(70<<8|(0xFF&-101))  #decimal ver of 70, -100
-                ser.write(struct('<B', rpm)) # < makes it little endian and flips it to #-100, 70
-                #https://pymotw.com/2/struct/
-            except (ser.SerialTimeoutException):
+                serialWrite(-101, 70) #write 70 to 101
+            #Serial port exception
+            except ser.SerialTimeoutException:
                 print("Serial is not open")
+            
+            time.sleep(0.001) #small delay TODO
  #       while(True):
  #           lcm_.handle()
-'''        print("start")
-        while 1:
-                reading = ser.read()#.decode('utf-8') #ser.read() for a byte
-                int_val = int.from_bytes(reading, byteorder='little', signed=True)
-                print(int_val)
-'''
+
 if __name__ == "__main__":
     main()
+    
